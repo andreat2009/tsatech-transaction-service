8dc99e709272a2d9eb7a5b6d83a58a5b18689515
andreaterrasi <andrea.terrasi78@gmail.com>
2026-03-16 09:58:15 +0100

    Normalize JWT role mapping for admin authorization

--- src/main/java/com/newproject/transaction/config/SecurityConfig.java
+++ src/main/java/com/newproject/transaction/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
         if (realmAccess != null && realmAccess.get("roles") instanceof List<?> roles) {
             for (Object role : roles) {
                 authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase(java.util.Locale.ROOT)));
             }
         }
 
@@ -56,6 +57,7 @@ public class SecurityConfig {
                     if (rolesObj instanceof List<?> roles) {
                         for (Object role : roles) {
                             authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+                            authorities.add(new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase(java.util.Locale.ROOT)));
                         }
                     }
                 }
